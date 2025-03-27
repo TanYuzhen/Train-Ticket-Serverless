9eb50aabaa5fd46c38a8ae6f9356553ba9aa81b4
TanYuzhen <1728040372@qq.com>
2025-03-27 15:45:50 +0800

    old cluster

--- train-ticket/reset.sh
+++ train-ticket/reset.sh
@@ -1,4 +1,17 @@
 helm uninstall owdev -n openwhisk
-kubectl delete -f ./deployment/Part01-database/*
+rm -rf /srv/nfs/kubedata/station
+rm -rf /srv/nfs/kubedata/auth
+rm -rf /srv/nfs/kubedata/config
+rm -rf /srv/nfs/kubedata/contacts
+rm -rf /srv/nfs/kubedata/insidePayment
+rm -rf /srv/nfs/kubedata/order
+rm -rf /srv/nfs/kubedata/payment
+rm -rf /srv/nfs/kubedata/price
+rm -rf /srv/nfs/kubedata/route
+rm -rf /srv/nfs/kubedata/security
+rm -rf /srv/nfs/kubedata/train
+rm -rf /srv/nfs/kubedata/travel
+rm -rf /srv/nfs/kubedata/user
+kubectl delete -f ./deployment/Part01-database/
 kubectl delete -f ./deployment/Part02-backend/service/
-kubectl delete -f ./deployment/Part03-frontend/*
+kubectl delete -f ./deployment/Part03-frontend/
